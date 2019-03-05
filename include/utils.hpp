d3b805c263277efe408164ae2c541dc27ae06595
kvmanohar22 <kvmanohar22@gmail.com>
2019-03-06 03:22:51 +0530

    add warping functionalities

--- include/utils.hpp
+++ include/utils.hpp
@@ -18,7 +18,9 @@ cv::Mat translate(float Tx = 0, float Ty = 0) {
     return T;
 }
 
-cv::Mat rotate(double theta=0, double Rx = 0, double Ry = 0) {
+cv::Mat rotate(double theta=0, 
+               double Rx = 0, 
+               double Ry = 0) {
     cv::Mat R = cv::getRotationMatrix2D(cv::Point(cv::Size(Rx, Ry)), theta, 1.0);
 
     return R;
@@ -34,8 +36,8 @@ cv::Mat scale(double Sx = 1.0, double Sy = 1.0) {
 
 cv::Mat compute_homography(std::vector<cv::KeyPoint> f1,
     std::vector<cv::KeyPoint> f2,
-    std::vector<cv::DMatch> matches
-) {
+    std::vector<cv::DMatch> matches) {
+
     int n_matches = matches.size();
 
     int n_rows = 2 * n_matches;
@@ -75,6 +77,7 @@ cv::Mat compute_homography(std::vector<cv::KeyPoint> f1,
     cv::Mat H = cv::Mat::eye(cv::Size(3, 3), CV_32F);
     H = Vt.row(Vt.rows-1).reshape(1, 3);
 
+    // Transforms the points from `f1` to `f2`
     return H;
 }
 
@@ -92,7 +95,8 @@ cv::Mat load_image(std::string file) {
 }
 
 void view_image(std::string win_name, cv::Mat img) {
-    cv::namedWindow(win_name, CV_WINDOW_FREERATIO);
+    cv::namedWindow(win_name, CV_WINDOW_NORMAL);
+    cv::resizeWindow(win_name, 640, 640);
     cv::imshow(win_name, img);
     cv::waitKey(0);
 }
@@ -111,6 +115,94 @@ std::vector<T> linspace(T a, T b, size_t N) {
         val += h;
     }
     return xs;
+}
+
+template <typename T>
+std::vector<T> arange(int start, int end) {
+    const int N = end - start + 1; 
+    std::vector<T> xs(N);
+    for (auto &itr : xs) {
+        itr = start++;
+    }
+    return xs;
+}
+
+void compute_spherical_warping(cv::Size2i out_shape, float f, cv::Mat &u, cv::Mat &v) {
+    const int h = out_shape.height;
+    const int w = out_shape.width;
+    cv::Mat xf  = cv::Mat::zeros(cv::Size(w, h), CV_32F);
+    cv::Mat yf  = cv::Mat::zeros(cv::Size(w, h), CV_32F);
+
+    for (int idx = 0; idx < xf.rows; ++idx) {
+        std::vector<float> vec = utils::arange<float>(0, w);
+        float data[vec.size()];
+        std::copy(vec.begin(), vec.end(), data);
+        cv::Mat row(cv::Size(xf.cols, 1), CV_32F, &data);
+        row.copyTo(xf.row(idx));
+    }
+    for (int idx = 0; idx < yf.cols; ++idx) {
+        std::vector<float> vec = utils::arange<float>(0, h);
+        float data[vec.size()];
+        std::copy(vec.begin(), vec.end(), data);
+        cv::Mat col(cv::Size(1, yf.rows), CV_32F, &data);
+        col.copyTo(yf.col(idx));
+    }
+
+    #ifdef TEST
+        std::cout << xf << std::endl;
+        std::cout << yf << std::endl;
+    #endif
+
+    for (int i = 0; i < h; ++i)
+        for (int j = 0; j < w; ++j)
+            xf.at<float>(i, j) = (xf.at<float>(i, j) - 0.5 * w) / f;
+    for (int i = 0; i < h; ++i)
+        for (int j = 0; j < w; ++j)
+            yf.at<float>(i, j) = (yf.at<float>(i, j) - 0.5 * h) / f;
+
+    #ifdef TEST
+        std::cout << xf << std::endl;
+        std::cout << yf << std::endl;
+    #endif
+
+    cv::Mat xhat(cv::Mat::zeros(cv::Size(w, h), CV_32F));
+    cv::Mat yhat(cv::Mat::zeros(cv::Size(w, h), CV_32F));
+    cv::Mat zhat(cv::Mat::zeros(cv::Size(w, h), CV_32F));
+
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            const float _xf = xf.at<float>(i, j); 
+            const float _yf = yf.at<float>(i, j); 
+            xhat.at<float>(i, j) = sin(_xf) * cos(_yf);
+            yhat.at<float>(i, j) = sin(_yf);
+            zhat.at<float>(i, j) = cos(_xf) * cos(_yf);
+        }
+    }
+
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            const float _zhat = zhat.at<float>(i, j); 
+            xhat.at<float>(i, j) /= _zhat;
+            yhat.at<float>(i, j) /= _zhat;
+        }
+    }
+
+    u = 0.5 * w + xhat * f;
+    v = 0.5 * h + yhat * f;
+}
+
+cv::Mat warp_local(cv::Mat img, cv::Mat &u, cv::Mat &v) {
+    cv::Mat warped;
+    cv::remap(img, warped, u, v, CV_INTER_LINEAR);
+
+    return warped.clone();
+}
+
+cv::Mat warp_spherical(cv::Mat img, float f) {
+    cv::Size size = img.size();
+    cv::Mat u, v;
+    compute_spherical_warping(size, f, u, v);
+    return warp_local(img, u, v);
 }
 
 } // namespace utils
